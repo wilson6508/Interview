4a2e9f2d8e5faeba30f7a77cd578cebfc5870937
wilson6508 <wilson650837@gmail.com>
2023-04-09 14:11:59 +0800

    串接API

--- src/components/project/StockPrice.vue
+++ src/components/project/StockPrice.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="text-center m-5">
     <div class="row mt-4">
-      <div class="col-3"></div>
-      <div class="col-6">
+      <div class="col-4"></div>
+      <div class="col-4">
         <div class="input-group">
-          <input type="text" class="form-control" placeholder="輸入股票代號查詢" @keyup.enter="query">
+          <input type="text" class="form-control" placeholder="輸入股票代號查詢" v-model="stockId" @keyup.enter="query">
           <div class="input-group-append">
             <button class="btn btn-primary" type="button" @click="query">查詢</button>
           </div>
         </div>
+        <span>(常用查詢代號: 0050 2330 2886)</span>
       </div>
-      <div v-show="test" class="spinner-border" role="status"></div>
-      <div class="col-3">
-      </div>
+      <div v-show="loading" class="spinner-border" role="status"></div>
+      <div class="col-4"></div>
     </div>
     <div class="row mt-5">
       <div class="col-2" v-for="(stock, idx) in arr" :key="idx">
         <div class="card bg-light mb-3">
           <div class="card-header d-flex justify-content-between">{{ stock.id }}
-            <button type="button" class="btn-close" aria-label="Close"></button>
+            <span class="btn-close" aria-label="Close" style="cursor: pointer" @click="clear(idx)"></span>
           </div>
           <div class="card-body">
             <h5 class="card-title">{{ stock.name }}</h5>
@@ -31,111 +31,49 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "StockPrice",
   data() {
     return {
-      test: true,
-      arr: [
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-        {
-          id: "2330",
-          name: "台積電",
-          price: "520.15",
-        },
-      ],
+      loading: false,
+      stockId: "",
+      arr: [],
     }
   },
   methods: {
     query() {
-      this.test = !this.test;
-      // const str = "請先關閉1個區塊 再進行查詢";
-      // alert(str);
-    }
+      const check = this.checkRegex();
+      if (!check) {
+        alert("無效的股票代號");
+        return;
+      }
+      this.loading = true;
+      const url = `http://192.168.93.100:7500/stockPrice/getSingle?stockId=${this.stockId}`;
+      axios.get(url)
+          .then(response => {
+            const {stockId, stockName, closePrice} = response.data;
+            this.arr.push({
+              id: stockId,
+              name: stockName,
+              price: closePrice.substring(0, closePrice.length - 2),
+            });
+          })
+          .catch(() => {
+            alert("查詢失敗");
+          })
+          .finally(() => {
+            this.loading = false;
+          });
+    },
+    clear(idx) {
+      this.arr.splice(idx, 1);
+    },
+    checkRegex() {
+      const regex = /^\d{4}[0-9a-zA-Z]{0,3}$/;
+      return regex.test(this.stockId);
+    },
   }
 };
 </script>
